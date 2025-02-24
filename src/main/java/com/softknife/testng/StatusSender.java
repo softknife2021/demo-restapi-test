3bcfaa0bf7ce720c9519c632dcaad6d3e29b656f
Alexander Matsaylo <softknife2021@gmail.com>
2025-02-24 12:34:02 -0800

    added curl test3

--- src/main/java/com/softknife/testng/StatusSender.java
+++ src/main/java/com/softknife/testng/StatusSender.java
@@ -9,17 +9,22 @@ import com.restbusters.rest.client.RestClientHelper;
 import com.restbusters.rest.model.HttpRestRequest;
 import com.softknife.testng.model.TestCaseStatus;
 import okhttp3.OkHttpClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.lang.invoke.MethodHandles;
 
 public class StatusSender {
 
     private static final ObjectMapper objectMapper = GlobalResourceManager.getInstance().getObjectMapper();
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private static DemoTestConfig config = DemoTestConfigResourceProvider.getInstance().getGlobalConfig();
     private static OkHttpClient okHttpClient = RestClientHelper.getInstance().buildBasicAuthClient(config.elasticUser(), config.elasticPass());
 
     public static void send(final TestCaseStatus testCaseStatus){
         HttpRestRequest httpRestRequest = new HttpRestRequest("POST", config.elasticHost() + config.elasticApp());
+        logger.info("elastic user {}, password {}", config.elasticUser(), config.elasticPass());
         try {
             httpRestRequest.setRequestBody(objectMapper.writeValueAsString(testCaseStatus));
         } catch (JsonProcessingException e) {
